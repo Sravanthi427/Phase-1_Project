528244cbd8a187ad8133967a2518f33ba4a4cf8f
Sravanthi Nedunoori <snedunoori@hotmail.com>
2020-10-03 02:03:38 +0000

    initial commit

--- multiplefiles/src/multiplefiles/Main.java
+++ multiplefiles/src/multiplefiles/Main.java
@@ -7,67 +7,63 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-int ch = 0;
-//		int r1=2,c1=3;
-//		int r2=3,c2=2;
-//		int firstMatrix[][]= {{3,-2,5},{3,0,4}};
-//		int secondMatrix[][]= {{2,3},{-9,0},{0,4}};
-		//BusinessLogic bl=new BusinessLogicImpl();
+		int ch = 0;
+		int ch1 = 0;
+		boolean innerswitch = false;
+		boolean outerswitch = false;
+
 		System.out.println("Welcome to Company Lockers example");
-		do {
+		if(!outerswitch) {
+		do{//outer switch
 			System.out.println("LockedMe.com Menu");
 			System.out.println("------------");
-			System.out.println("1)Return Files");
-			System.out.println("2)Add File");
-			System.out.println("3)Delete File");
-			System.out.println("4)Search File");
-			System.out.println("5)Return to Main Menu");
+			System.out.println("1)List Files in ascending order");
+			System.out.println("2)Add File, Delete File and Search File");
+			System.out.println("3)Return to Main Menu");
+			System.out.println("Please enter your appropriate choice(1-3)");
 			
-			System.out.println("Please enter your appropriate choice(1-5)");
 			try {
 				ch = Integer.parseInt(sc.nextLine());
 			} catch (NumberFormatException e) {
 
 			}
+			
 			switch (ch) {
 			case 1:
+				System.out.println("This option lists all the files in root directory.....");
 				ListFiles lf = new ListFiles();
 				lf.listFilesAsc();
-				System.out.println("This option lists all the files in root directory.....");
-				break;
+			 break;
 			case 2:
-				FileUtil f = new FileUtil();
-				f.addFile();
-				System.out.println("This option add files in root directory.....");
-                break;
-			case 3:
-				FileUtilDelete fd = new FileUtilDelete();
-				fd.deleteFile();
-				System.out.println("This option delete files in root directory.....");
-		        break;
-			case 4:
-				SearchFile sf = new SearchFile();
-				sf.searchFileList();
-				System.out.println("This option search files in root directory......");
-				break;
-			case 5:
-				System.out.println("Return to Main Menu");
-				break;
 				
-			default:System.out.println("Invalid Choice... Choice should and must be between 1-5 only");
+				System.out.println("file Operations");
+				ProjectMain pm = new ProjectMain();
+				pm.fileOperations();
 				break;
-			}
-		} while (ch != 5);
-
+			case 3:
+				System.out.println("Exiting the application");
+				return;
+			
+			default:System.out.println("Invalid Choice... Choice should and must be between 1-3 only");
+			break;
+				
+		} 
+			
+			
+		}
+		while (ch != 3);
+		}
 	}
 	
-//	public static void printResultMatrice(int[][]results) {
-//		for(int[]row :results) {
-//			for(int column:row) {
-//				System.out.print(column+" ");
-//			}
-//			System.out.println();
-//		}
-//	}
-
 }
+
+
+
+
+	
+
+	
+	
+
+
+
--- multiplefiles/src/multiplefiles/ProjectMain.java
+++ multiplefiles/src/multiplefiles/ProjectMain.java
@@ -1,24 +1,53 @@
-//package multiplefiles;
-//
-//public class ProjectMain {
-//
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//		
-////FileUtil f = new FileUtil();
-////f.addFile();
-//		
-////		FileUtilDelete fd = new FileUtilDelete();
-////		fd.deleteFile();
-//		
-////	   ListFiles lf = new ListFiles();
-////	   lf.listFilesAsc();
-//	   
-//	   
-//		
-//		SearchFile sf = new SearchFile();
-//		sf.searchFileList();
-//
-//	}
-//
-//}
+package multiplefiles;
+
+//import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ProjectMain {
+	
+public static void fileOperations()
+{
+	int ch1 = 0;
+	Scanner sc = new Scanner(System.in);
+
+	
+	do{//inner switch
+		
+		System.out.println("1)Add File");
+		System.out.println("2)Delete File");
+		System.out.println("3)Search File");
+		System.out.println("4)Return to Main Menu");
+		System.out.println("Please enter your appropriate choice(1-4)");
+		ch1 = sc.nextInt();
+		
+	switch(ch1) {
+	case 1 :
+		System.out.println("This option add files in root directory.....");
+		FileUtil f = new FileUtil();
+		f.addFile();
+		break;
+	case 2: System.out.println("This option delete files in root directory.....");
+		FileUtilDelete fd = new FileUtilDelete();
+		fd.deleteFile();
+		break;
+	case 3:
+		System.out.println("This option search files in root directory......");
+		SearchFile sf = new SearchFile();
+		sf.searchFileList();
+		break;
+	case 4: 
+		System.out.println("Return to main menu");
+		return;
+	default:
+		System.out.println("Entered invalid choice");
+	
+}
+
+	}while(ch1!=4);
+}
+}
+
